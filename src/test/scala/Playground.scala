0427ce47e1801d1ba4927cc19a382505204690d8
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-30 13:23:42 +0800

    simplify

--- src/test/scala/Playground.scala
+++ src/test/scala/Playground.scala
@@ -1,27 +1,21 @@
 import org.junit.Test
 
 def cutter[A](n: Integer): List[A] => (List[A], List[A]) =
-  (l: List[A]) => (l.take(n), l.drop(n))
-
-def condition[A](n: Integer): List[A] => Boolean =
-  (l: List[A]) => (l.length >= n)
+  (l: List[A]) =>
+    if (l.length >= n) (l.take(n), l.drop(n))
+    else (List.empty, List.empty)
 
 def f[A](
-    cond: List[A] => Boolean,
-    c: List[A] => (List[A], List[A])
-): List[A] => List[List[A]] = {
-  def fm(l: List[A]): List[List[A]] = {
-    if (cond(l)) {
-      val (ls: List[A], rest: List[A]) = c(l)
-      ls :: fm(rest)
-    } else List.empty
-  }
-  fm
+    c: List[A] => (List[A], List[A]),
+    l: List[A]
+): List[List[A]] = {
+  val (ls: List[A], rest: List[A]) = c(l)
+  if (ls.isEmpty) List.empty else ls :: f(c, rest)
 }
 
 class Playground {
   @Test def playground(): Unit = {
-    val chunk_by = (lst) => f(condition(2), cutter(2))(lst)
+    val chunk_by = lst => f(cutter(2), lst)
     println(chunk_by(List(1, 2, 3)))
     println(chunk_by(List(1, 2, 3, 4, 5)))
   }
